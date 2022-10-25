76dfc6db15b3d84e939ebf5a6b512bfa0db43c0e
Darshan <darshanmalaviya64@gmail.com>
2022-10-25 19:25:14 +0530

    excercise view completed

--- SevenMinWorkout/app/src/main/java/com/example/sevenminworkout/ExerciseActivity.kt
+++ SevenMinWorkout/app/src/main/java/com/example/sevenminworkout/ExerciseActivity.kt
@@ -3,66 +3,138 @@ package com.example.sevenminworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.sevenminworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    private var binding:ActivityExerciseBinding?=null
+    private var binding: ActivityExerciseBinding? = null
 
-    private var restTimer:CountDownTimer?=null
-    private var restProgress=0
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+
+    private var exersiseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityExerciseBinding.inflate(layoutInflater);
+        binding = ActivityExerciseBinding.inflate(layoutInflater);
         setContentView(binding?.root)
 
         setSupportActionBar(binding?.toolbarExercise)
-        if (supportActionBar!=null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
+
+        exersiseList = Constants.defaultExerciseList()
+
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
         setupRestView()
     }
 
-    private fun setRestProgressBar(){
-        binding?.progressBar?.progress=restProgress
-        restTimer=object:CountDownTimer(10000,1000){
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
-                binding?.progressBar?.progress=10-restProgress
-                binding?.tvTimer?.text=(10-restProgress).toString()
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
             }
 
             override fun onFinish() {
-                binding?.progressBar?.progress=0
-                binding?.tvTimer?.text="0"
-                Toast.makeText(
-                    this@ExerciseActivity,
-                    "Here now we will start the exercise",
-                    Toast.LENGTH_SHORT
-                ).show()
+                binding?.progressBar?.progress = 0
+                binding?.tvTimer?.text = "0"
+                currentExercisePosition++
+//                Toast.makeText(
+//                    this@ExerciseActivity,
+//                    "Here now we will start the exercise",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+                setupExerciseView()
             }
         }.start()
     }
 
-    fun setupRestView(){
-        if(restTimer!=null){
+    private fun setExerciseProgressBar() {
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                binding?.progressBarExercise?.progress = 0
+                binding?.tvTimerExercise?.text = "0"
+//                Toast.makeText(
+//                    this@ExerciseActivity,
+//                    "30 secs are over let's go to the rest view",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+                if (currentExercisePosition < exersiseList!!.size - 1) {
+                    setupRestView()
+                } else {
+                    Toast.makeText(
+                        this@ExerciseActivity,
+                        "Congo",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }.start()
+    }
+
+
+    private fun setupRestView() {
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.tvTitle?.visibility = View.VISIBLE
+        binding?.tvExerciseName?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.INVISIBLE
+        binding?.ivImage?.visibility = View.INVISIBLE
+        if (restTimer != null) {
             restTimer?.cancel()
-            restProgress=0
+            restProgress = 0
         }
         setRestProgressBar()
     }
 
+    private fun setupExerciseView() {
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvExerciseName?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.ivImage?.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        binding?.ivImage?.setImageResource(exersiseList!![currentExercisePosition].getImage())
+        binding?.tvExerciseName?.text = exersiseList!![currentExercisePosition].getName()
+        setExerciseProgressBar()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        if(restTimer!=null){
+        if (restTimer != null) {
             restTimer?.cancel()
-            restProgress=0
+            restProgress = 0
         }
-        binding=null
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        binding = null
     }
 }
