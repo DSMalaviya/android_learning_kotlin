1c829d973fb6959a34c0cef33858a1850f20ebc8
Darshan <darshanmalaviya64@gmail.com>
2022-11-09 20:37:27 +0530

    last cmt

---
+++ greatPlaces/app/src/main/java/com/example/greatplaces/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.greatplaces
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.greatplaces.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent=Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
